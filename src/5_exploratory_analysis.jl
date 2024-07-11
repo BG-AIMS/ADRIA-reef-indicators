a490d22ba2d95162ef6b9490c9b5dc08b658a145
Benjamin Grier <b.grier@aims.gov.au>
2024-07-11 16:08:09 +1000

    consistency and cleanup changes

--- src/5_exploratory_analysis.jl
+++ src/5_exploratory_analysis.jl
@@ -17,23 +17,12 @@ import GeoDataFrames as GDF
 # If previous scripts have never been run or updated:
 # include("1_initial_results.jl")
 # include("3_Subregion_correlation.jl")
-# include("4_Bioregion_correlation.jl")
-# include("5_analysis_context_layers.jl")
+# include("4_analysis_context_layers.jl")
 
 # Load updated context layer data
-context_layers = GDF.read("../data/analysis_context_layers.gpkg",)
+context_layers = GDF.read("data/analysis_context_layers.gpkg",)
 
 target_reefs_context = context_layers[context_layers.RME_UNIQUE_ID .∈ [target_reefs], :]
-
-hist(context_layers.out_in_strength; bins=[0,0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,3.5,4,5,6])
-vlines!(1, color=:red)
-
-hist(context_layers.out_in_count, bins=[0,0.5,1,1.5,2,2.5,3,3.5,4,5,7.5,10,12.5,15,20,25,30,35,40,50,60,70,80,90,150])
-vlines!(1, color=:red)
-
-hist(context_layers, bins=[0,0.5,1,1.5,2,2.5,3,3.5,4,5,7.5,10,12.5,15,20,25,30,35,40,50,60,70,80,90,150])
-
-
 
 # Comparing the distribution of gbr wide connectivity metrics to those found in target_reefs
 
@@ -86,48 +75,53 @@ hist(target_reefs_context[(target_reefs_context.bioregion .== "Outer Shelf Reefs
 hist(context_layers.area, bins=30)
 hist(target_reefs_context.area)
 
+# Load rel_cover data and identify reefs that crash in the first timeseries year
+rs = ADRIA.load_results("outputs/ADRIA-out/ReefMod Engine__RCPs_45__2024-06-19_10_58_55_647")
+tac = ADRIA.metrics.total_absolute_cover(rs)
+tac_sites = Float64.(mapslices(median, tac, dims=[:scenarios]))
+tac_sites_reduced = tac_sites[timesteps=2:79]
+rel_cover = Float64.(mapslices(relative_site_cover, tac_sites_reduced, dims=[:timesteps]))
+crash_reefs = collect(getAxis("sites", rel_cover).val)[rel_cover.data[2,:] .< 0.5]
 
 no_crash_gbr = context_layers[context_layers.UNIQUE_ID .∉ [crash_reefs],:]
-filtered_bior_context = no_crash_gbr[no_crash_gbr.bioregion .∈ [unique(target_reefs_context.bioregion)], :]
-filtered_subr_context = no_crash_gbr[no_crash_gbr.closest_port .∈ [unique(target_reefs_context.closest_port)], :]
+filtered_bior = no_crash_gbr[no_crash_gbr.bioregion .∈ [unique(target_reefs_context.bioregion)], :]
+filtered_subr = no_crash_gbr[no_crash_gbr.closest_port .∈ [unique(target_reefs_context.closest_port)], :]
 
-glm_allreefs = glm(@formula(target_cat ~ total_comb + so_to_si + initial_coral_cover + mean_dhw + bioregion_count + dhw_cor ), no_crash_gbr, Binomial())
+glm_allreefs = glm(@formula(target_reefs ~ total_comb + so_to_si + initial_coral_cover + mean_dhw + bioregion_count + dhw_cor ), no_crash_gbr, Binomial())
 glm_bioregions = glm(@formula(target_cat ~ total_comb + so_to_si + mean_dhw + initial_coral_cover + mean_dhw + bioregion_count + dhw_cor +0), bioregion_only, Binomial())
 glm_subregions = glm(@formula(target_cat ~ total_comb + so_to_si + initial_coral_cover + bioregion_count), filtered_subr_context, Binomial())
 
 aic(glm_allreefs), aic(glm_bioregions), aic(glm_subregions)
 
-glmm_form = @formula(target_cat ~ total_comb + so_to_si + initial_coral_cover + mean_dhw + bioregion_count + dhw_cor + (1|bioregion) + (dhw_cor|bioregion))
-glmm_fit = fit(MixedModel, glmm_form, filtered_bior_context, Binomial(), ProbitLink())
-aic(glmm_fit) # seems to be an improvement when (1|bioregion/closest_port) is used
+glmm_form = @formula(target_reefs ~ total_comb + so_to_si + initial_coral_cover + mean_dhw + bioregion_count + dhw_cor + (1|bioregion))
+glmm_fit = fit(MixedModel, glmm_form, no_crash_gbr, Binomial(), ProbitLink())
+aic(glmm_fit) # seems to be an improvement when (1|bioregion) is used
 
-
-
-df = DataFrame(rel_dhw.data, collect(getAxis("locs", rel_dhw).val))
+df = DataFrame(rel_cover.data, collect(getAxis("sites", rel_cover).val))
 df.year = [string(i+1) for i in 1:size(df,1)]
 select!(df, :year, Not(:year))
 
 data = permutedims(df, 1, "UNIQUE_ID")
-data = data[data.UNIQUE_ID .∈ [filtered_bior_context.UNIQUE_ID],:]
-data = leftjoin(data, filtered_bior_context[:, [:UNIQUE_ID, :target_cat, :bioregion]], on=:UNIQUE_ID)
-data.target_cat = ifelse.(data.target_cat, "target", "non-target")
+data = data[data.UNIQUE_ID .∈ [filtered_bior.UNIQUE_ID],:]
+data = leftjoin(data, filtered_bior[:, [:UNIQUE_ID, :target_reefs, :bioregion]], on=:UNIQUE_ID)
+data.target_reefs = ifelse.(data.target_reefs, "target", "non-target")
 
-f, ax = plot_lines(data, :target_cat, 2, 15, "Year", "Proportion of reef cover Year2", 0.2)
+f, ax = plot_lines(data, :target_reefs, 2, 15, "Year", "Proportion of reef cover Year2", 0.2)
 f, ax = plot_lines(data[data.UNIQUE_ID .∈ [target_reefs_context.UNIQUE_ID],:], :bioregion, 2, 15, "Year", "Proportion of reef cover Year2", 0.2)
 hlines!(1, color=:green)
 save("figs/all_reefs_initial_man_areas.png", f)
 
 
-unique(filtered_bior_context.bioregion)
-bioregion_only = filtered_bior_context[(filtered_bior_context.bioregion .== ["Far Northern Protected Mid Shelf Reefs and Shoals"]),:]
+unique(filtered_bior.bioregion)
+bioregion_only = filtered_bior[(filtered_bior.bioregion .== ["Far Northern Protected Mid Shelf Reefs and Shoals"]),:]
 df = DataFrame(rel_cover.data, collect(getAxis("sites", rel_cover).val))
 df.year = [string(i+1) for i in 1:size(df,1)]
 select!(df, :year, Not(:year))
 
 data = permutedims(df, 1, "UNIQUE_ID")
 data = data[data.UNIQUE_ID .∈ [bioregion_only.UNIQUE_ID],:]
-data = leftjoin(data, bioregion_only[:, [:UNIQUE_ID, :target_cat]], on=:UNIQUE_ID)
-data.target_cat = ifelse.(data.target_cat, "target", "non-target")
+data = leftjoin(data, bioregion_only[:, [:UNIQUE_ID, :target_reefs]], on=:UNIQUE_ID)
+data.target_cat = ifelse.(data.target_reefs, "target", "non-target")
 
 lagged_data = data[data.target_cat .== ["non-target"],:]
 select!(lagged_data, :UNIQUE_ID, Not(:2, :3, :4, :5, :6))
